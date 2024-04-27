401356ae22bac69bda7770263a9a70e8ae0b7d46
Hridoy1107 <hridoyx1107@gmail.com>
2024-04-27 21:49:33 +0600

    10th

--- src/mainPages/Header.jsx
+++ src/mainPages/Header.jsx
@@ -80,10 +80,12 @@ const Header = () => {
                                 <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                                 </div>
+                                <div>
                                 <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 gap-y-1 shadow bg-base-100 rounded-box w-[135px]">
                                     <li><a onClick={handleLogOut} className="btn w-[120px] h-[48px] bg-[red] hover:bg-[red] text-[#FFFFFF]">Log out</a></li>
                                     <li><NavLink to="/user-profile" className="btn w-[120px] h-[48px] bg-[blue] hover:bg-[blue] text-[#FFFFFF]">Your Profile</NavLink></li>
                                 </ul>
+                                </div>
                             </div>
                         </> :
                         <>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,30 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/free-mode';
-
 import { FreeMode, Autoplay } from 'swiper/modules';
+import { Typewriter } from 'react-simple-typewriter'
 
 const Home = () => {
     return (
         <>
+            <div className="mt-5">
+                <h1 className=" text-4xl text-emerald-600">
+                    <span style={{ color: 'emerald', fontWeight: 'semibold' }}>
+                        <Typewriter
+                            words={['Collect Moments, Not Things']}
+                            loop={50}
+                            cursor
+                            cursorStyle='_'
+                            typeSpeed={70}
+                            deleteSpeed={50}
+                            delaySpeed={1000}
+                        />
+                    </span>
+                </h1>
+                <h1 className=" text-2xl mt-1 px-10 text-center font-medium text-sky-600">
+                    The world beckons with hidden wonders and unforgettable experiences.  Travel is more than just sightseeing; it's a chance to broaden your horizons and collect memories that last a lifetime.  Embark on an adventure, discover new cultures, and lose yourself in the beauty our planet has to offer.
+                </h1>
+            </div>
             <div className="mt-10">
                 <Swiper
                     slidesPerView={1}
@@ -20,12 +38,20 @@ const Home = () => {
                     }}
                     modules={[FreeMode, Autoplay]}
                 >
-                    <SwiperSlide><img className="w-full h-[580px] rounded-2xl" src="https://i.ibb.co/B3zQr9P/2147732543.jpg" /></SwiperSlide>
-                    <SwiperSlide><img className="w-full h-[580px] rounded-2xl" src="https://i.ibb.co/wMkysRS/29599.jpg" /></SwiperSlide>
-                    <SwiperSlide><img className="w-full h-[580px] rounded-2xl" src="https://i.ibb.co/RPDW2sV/28417.jpg" /></SwiperSlide>
-                    <SwiperSlide><img className="w-full h-[580px] rounded-2xl" src="https://i.ibb.co/kqpBdNT/1356.jpg" /></SwiperSlide>
+                    <SwiperSlide><img className="w-full h-[600px] rounded-2xl" src="https://i.ibb.co/B3zQr9P/2147732543.jpg" /></SwiperSlide>
+                    <SwiperSlide><img className="w-full h-[600px] rounded-2xl" src="https://i.ibb.co/wMkysRS/29599.jpg" /></SwiperSlide>
+                    <SwiperSlide><img className="w-full h-[600px] rounded-2xl" src="https://i.ibb.co/RPDW2sV/28417.jpg" /></SwiperSlide>
+                    <SwiperSlide><img className="w-full h-[600px] rounded-2xl" src="https://i.ibb.co/kqpBdNT/1356.jpg" /></SwiperSlide>
                 </Swiper>
             </div>
+            <div className="mt-5">
+                <h1 className="animate__animated animate__zoomInDown animate__repeat-1 text-5xl font-bold text-emerald-500
+            animate-text">Your Travel</h1>
+                <h1 className=" text-2xl mt-2 px-10 text-center font-medium text-rose-800">
+                    The call of adventure whispers on the wind. Here at <span className="font-bold text-emerald-400">Your Travel</span>, we believe travel isn't just a vacation, it's a chance to broaden your horizons and collect memories that last a lifetime.  Explore our curated destinations and embark on unforgettable journeys that will enrich your soul and leave you yearning for more.
+                </h1>
+            </div>
+            
         </>
     );
 };
