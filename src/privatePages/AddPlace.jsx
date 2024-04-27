5459609af5984a0e0f27ff4f97be7712e8fc322d
Hridoy1107 <hridoyx1107@gmail.com>
2024-04-28 04:13:29 +0600

    15th

--- src/privatePages/AddPlace.jsx
+++ src/privatePages/AddPlace.jsx
@@ -1,49 +1,55 @@
+// import Swal from 'sweetalert2'
+
 const AddPlace = () => {
 
-    const handleAddCoffee = event => {
+    const handleAddPlace = event => {
         event.preventDefault();
 
         const form = event.target;
 
         const name = form.name.value;
-        const quantity = form.quantity.value;
-        const supplier = form.supplier.value;
-        const taste = form.taste.value;
-        const category = form.category.value;
-        const details = form.details.value;
+        const email = form.email.value;
+        const place = form.place.value;
+        const country = form.country.value;
+        const location = form.location.value;
+        const cost = form.cost.value;
+        const season = form.season.value;
+        const time = form.time.value;
+        const visitors = form.visitors.value;
         const photo = form.photo.value;
+        const details = form.details.value;
 
-        const newCoffee = { name, quantity, supplier, taste, category, details, photo }
+        const newPlace = { name, email, place, country, location, cost, season, time, visitors, photo, details }
 
-        console.log(newCoffee);
+        console.log(newPlace);
 
         // send data to the server
-        fetch('http://localhost:5000/coffee', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(newCoffee)
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                if(data.insertedId){
-                    Swal.fire({
-                        title: 'Success!',
-                        text: 'Coffee Added Successfully',
-                        icon: 'success',
-                        confirmButtonText: 'Cool'
-                      })
-                }
-            })
+        // fetch('http://localhost:5000/place', {
+        //     method: 'POST',
+        //     headers: {
+        //         'content-type': 'application/json'
+        //     },
+        //     body: JSON.stringify(newPlace)
+        // })
+        //     .then(res => res.json())
+        //     .then(data => {
+        //         console.log(data);
+        //         if(data.insertedId){
+        //             Swal.fire({
+        //                 title: 'Success!',
+        //                 text: 'Place Added Successfully',
+        //                 icon: 'success',
+        //                 confirmButtonText: 'Cool'
+        //               })
+        //         }
+        //     })
     }
 
     return (        
         <>
             <div className="bg-slate-100 py-12 px-24 mt-10 rounded-2xl">
                 <h2 className="mb-6 text-3xl font-bold text-teal-700">Add A Place</h2>
-                <form onSubmit={handleAddCoffee}>
+                <form onSubmit={handleAddPlace}>
                     <div className="flex mb-8">
                         <div className="form-control w-1/2">
                             <label className="label">
